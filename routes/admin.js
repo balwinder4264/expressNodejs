c8d112e3c6c23d4d8780b4d2b1e286effe6ae2a4
balwinder4264 <balwindersingh4264@gmail.com>
2021-05-29 19:40:22 -0400

    connect with js engine

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,28 @@
-const express = require("express");
+const path = require('path');
 
-const path = require('path')
+const express = require('express');
 
-const routePath = require('../util/path')
-const router  = express.Router();
-// const app=express();
-// app and router functions works in same way so use any here i am using router
-router.get("/add-product",(req,res,next)=>{
-    console.log("it is another middleware ")
-    res.sendFile(path.join(routePath,"views",'add-product.html'))
+
+const router = express.Router();
+
+const products = [];
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
-router.post("/add-product",(req,res,next)=>{
-    console.log(req.body)
-   res.redirect("/")
-//    next();//jump to next middleware in line 
-})
 
-module.exports=router;
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,22 @@
-const express = require("express");
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
 const router = express.Router();
-const path = require('path')
 
-const routePath = require('../util/path')
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
+});
 
-router.get("/",(req,res,next)=>{
-    // console.log(req.body)
-    res.sendfile(path.join(routePath,'views','shop.html'))
-//    next();//jump to next middleware in line 
-})
-
-
-module.exports =router
+module.exports = router;
